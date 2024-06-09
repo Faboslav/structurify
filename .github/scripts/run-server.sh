474ebb1c0d89d1f846994a26fe325ea2c0cb7ed4
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-06-09 19:00:47 +0200

    Fixed CI

--- .github/scripts/run-server.sh
+++ .github/scripts/run-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p $1/run && echo "eula=true" > $1/run/eula.txt
-
 ./gradlew $1:runServer --no-daemon --args="nogui" 2>&1 | tee gradle_server_output.txt &
 
 SUCCESS_PATTERN='For help, type "help"'
