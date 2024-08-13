5421735acfd258fe91f00b7a3d794d953ab20e45
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-08-13 14:45:28 +0200

    Improved build

--- .github/scripts/parse-gradle-properties.sh
+++ .github/scripts/parse-gradle-properties.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+version=$1
+
 while IFS='=' read -r key value; do
     # Trim leading/trailing whitespace from key and value
     key=$(echo "$key" | awk '{$1=$1;print}')
@@ -11,3 +13,15 @@ while IFS='=' read -r key value; do
     # Output the key-value pair to the GitHub output
     echo "${key}=${value}" >> "$GITHUB_OUTPUT"
 done < gradle.properties
+
+while IFS='=' read -r key value; do
+    # Trim leading/trailing whitespace from key and value
+    key=$(echo "$key" | awk '{$1=$1;print}')
+    value=$(echo "$value" | awk '{$1=$1;print}')
+
+    # Convert key to uppercase and replace non-alphanumeric characters with underscores
+    key=$(echo "$key" | tr '[:lower:]' '[:upper:]' | tr -c '[:alnum:]' '_')
+
+    # Output the key-value pair to the GitHub output
+    echo "${key}=${value}" >> "$GITHUB_OUTPUT"
+done < versions/"${version}"/gradle.properties
