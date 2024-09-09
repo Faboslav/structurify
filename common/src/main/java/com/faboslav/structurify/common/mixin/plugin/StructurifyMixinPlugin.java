ce157980df6d8c243b7abdef0ffdc71d9239a58c
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-09-09 10:12:52 +0200

    Removed from neo

--- common/src/main/java/com/faboslav/structurify/common/mixin/plugin/StructurifyMixinPlugin.java
+++ common/src/main/java/com/faboslav/structurify/common/mixin/plugin/StructurifyMixinPlugin.java
@@ -24,10 +24,7 @@ public class StructurifyMixinPlugin implements IMixinConfigPlugin
 			return this.isClassAvailable("com.telepathicgrunt.repurposedstructures.world.structures.placements.AdvancedRandomSpread");
 		}
 
-		if (
-			mixinClassName.equals("com.faboslav.structurify.forge.mixin.compat.StructureGelApiModifySpreadMixin")
-			|| mixinClassName.equals("com.faboslav.structurify.neoforge.mixin.compat.StructureGelApiModifySpreadMixin")
-		) {
+		if (mixinClassName.equals("com.faboslav.structurify.forge.mixin.compat.StructureGelApiModifySpreadMixin")) {
 			return this.isClassAvailable("com.legacy.structure_gel.api.structure.GridStructurePlacement");
 		}
 
