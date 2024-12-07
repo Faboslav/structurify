a12258250ca8e1207fbfb34def2117355f1b27bf
Faboslav <fabianek.ales@gmail.com>
2024-12-07 16:31:15 +0100

    Added mod_loader_alias

--- .github/scripts/generate-run-matrix.sh
+++ .github/scripts/generate-run-matrix.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Initialize the matrix JSON with the 'include' key
 matrix_content="{\"include\":["
-
-# Extract enabled platforms from gradle.properties
 enabled_platforms=$(awk -F= '/stonecutter_enabled_platforms/{print $2}' gradle.properties | tr -d ' ')
 
-# Iterate over each platform and gather versions
 for platform in $(echo $enabled_platforms | tr ',' ' '); do
+  if [[ "$platform" == "forge" ]]; then
+    mod_loader_alias="lexforge"
+  else
+    mod_loader_alias="$platform"
+  fi
+
   versions=$(awk -F= '/stonecutter_enabled_'$platform'_versions/{print $2}' gradle.properties | tr -d ' ')
   for version in $(echo $versions | tr ',' ' '); do
-    # Create each entry with a JSON object for each combination
-    matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"script\":\"client\"},"
-    matrix_content+="$matrix_entry"
-    matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"script\":\"server\"},"
+    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\"},"
     matrix_content+="$matrix_entry"
   done
 done
 
-# Remove the trailing comma and close the JSON object
 matrix_content="${matrix_content%,}]}"
-# Output the matrix content for debugging
 echo "Generated matrix: $matrix_content"
-# Set the output for GitHub Actions
 echo "::set-output name=matrix::$matrix_content"
