ed841ec9acba43ce9f016267db163a2d42f3519a
Faboslav <fabianek.ales@gmail.com>
2024-12-07 18:31:07 +0100

    Added missing impl

--- forge/src/main/java/com/faboslav/structurify/common/util/forge/PlatformImpl.java
+++ forge/src/main/java/com/faboslav/structurify/common/util/forge/PlatformImpl.java
@@ -1,11 +1,18 @@
 package com.faboslav.structurify.common.util.forge;
 
+import com.faboslav.structurify.common.Structurify;
 import net.minecraftforge.fml.ModList;
+import org.jetbrains.annotations.Nullable;
 
 public final class PlatformImpl
 {
 	public static boolean isModLoaded(String modId) {
 		return ModList.get().isLoaded(modId);
+	}
+
+	@Nullable
+	public static String getModVersion() {
+		return ModList.get().getModContainerById(Structurify.MOD_ID).map(modContainer -> modContainer.getModInfo().getVersion().toString()).orElse(null);
 	}
 
 	private PlatformImpl() {
