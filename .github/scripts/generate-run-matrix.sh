7782899da85787587ceba95926d57dd0ac3a9325
Faboslav <fabianek.ales@gmail.com>
2024-12-07 16:59:25 +0100

    Added yacl version to matrix

--- .github/scripts/generate-run-matrix.sh
+++ .github/scripts/generate-run-matrix.sh
@@ -12,7 +12,8 @@ for platform in $(echo $enabled_platforms | tr ',' ' '); do
 
   versions=$(awk -F= '/stonecutter_enabled_'$platform'_versions/{print $2}' gradle.properties | tr -d ' ')
   for version in $(echo $versions | tr ',' ' '); do
-    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\"},"
+    yacl_version=$(awk -F= '/yacl_version/{print $2}' versions/"${version}"/gradle.properties | tr -d ' ')
+    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\",\"yacl_version\":\"$yacl_version},"
     matrix_content+="$matrix_entry"
   done
 done
