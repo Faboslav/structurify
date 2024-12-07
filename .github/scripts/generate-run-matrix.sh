09ea8d17c320ab118835386c6196ed4172b77e71
Faboslav <fabianek.ales@gmail.com>
2024-12-07 17:14:13 +0100

    Fixed matrix

--- .github/scripts/generate-run-matrix.sh
+++ .github/scripts/generate-run-matrix.sh
@@ -13,7 +13,7 @@ for platform in $(echo $enabled_platforms | tr ',' ' '); do
   versions=$(awk -F= '/stonecutter_enabled_'$platform'_versions/{print $2}' gradle.properties | tr -d ' ')
   for version in $(echo $versions | tr ',' ' '); do
     yacl_version=$(awk -F= '/yacl_version/{print $2}' versions/"${version}"/gradle.properties | tr -d ' ')
-    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\",\"yacl_version\":\"$yacl_version},"
+    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\",\"yacl_version\":\"$yacl_version\"},"
     matrix_content+="$matrix_entry"
   done
 done
