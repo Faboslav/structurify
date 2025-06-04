cfb0a6bb25c63f49957535676eec889f66561478
Faboslav <fabianek.ales@gmail.com>
2025-06-04 10:32:27 +0100

    Added import

--- common/src/main/java/com/faboslav/structurify/common/mixin/AbstractWidgetInvoker.java
+++ common/src/main/java/com/faboslav/structurify/common/mixin/AbstractWidgetInvoker.java
@@ -3,6 +3,10 @@ package com.faboslav.structurify.common.mixin;
 import net.minecraft.client.gui.components.AbstractWidget;
 import org.spongepowered.asm.mixin.Mixin;
 
+//? <1.21 {
+/*import org.spongepowered.asm.mixin.gen.Accessor;
+*///?}
+
 @Mixin(AbstractWidget.class)
 public interface AbstractWidgetInvoker
 {
