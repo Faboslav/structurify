1a55befbcb5bb8ec63a06e0feb08e817dc0cb4bf
Faboslav <fabianek.ales@gmail.com>
2025-08-06 15:58:41 +0200

    bumped versions

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("dev.kikugie.stonecutter")
 
-    id("net.neoforged.moddev") version "2.0.78" apply false
-    id("fabric-loom") version "1.10-SNAPSHOT" apply false
+    id("net.neoforged.moddev") version "2.0.106" apply false
+	id("fabric-loom") version "1.10-SNAPSHOT" apply false
 }
 
 stonecutter active "1.21.8" /* [SC] DO NOT EDIT */
