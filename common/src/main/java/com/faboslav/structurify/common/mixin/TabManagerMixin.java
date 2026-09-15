e00c04b4c286a07ba7bf0c615f10e7507cf3b204
Faboslav <fabianek.ales@gmail.com>
2026-09-15 20:27:54 +0200

    Fixed mixin

--- common/src/main/java/com/faboslav/structurify/common/mixin/TabManagerMixin.java
+++ common/src/main/java/com/faboslav/structurify/common/mixin/TabManagerMixin.java
@@ -1,31 +1,66 @@
 package com.faboslav.structurify.common.mixin;
 
 import com.faboslav.structurify.common.api.StructurifyCategoryTab;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.tabs.Tab;
 import net.minecraft.client.gui.components.tabs.TabManager;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.function.Consumer;
 
 @Mixin(TabManager.class)
 public abstract class TabManagerMixin
 {
-	@WrapOperation(
-		method = "setCurrentTab",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/gui/components/tabs/Tab;visitChildren(Ljava/util/function/Consumer;)V"
-		)
-	)
-	private void structurify$visitChildren(Tab tab, Consumer<AbstractWidget> consumer, Operation<Void> original) {
-		original.call(tab, consumer);
+	@Shadow
+	@Final
+	private Consumer<AbstractWidget> addWidget;
 
-		if (tab instanceof StructurifyCategoryTab categoryTab && categoryTab.structurify$getToggleGroupsButton() != null) {
-			consumer.accept(categoryTab.structurify$getToggleGroupsButton());
+	@Shadow
+	@Final
+	private Consumer<AbstractWidget> removeWidget;
+
+	@Shadow
+	@Nullable
+	private Tab currentTab;
+
+	@WrapMethod(
+		//? if >= 26.2 {
+		method = "setCurrentTab(Lnet/minecraft/client/gui/components/tabs/Tab;ZZ)V"
+		//?} else {
+		/*method = "setCurrentTab(Lnet/minecraft/client/gui/components/tabs/Tab;Z)V"
+		*///?}
+	)
+	private void structurify$setCurrentTab(
+		Tab tab,
+		boolean playClickSound,
+		//? if >= 26.2 {
+		boolean flag,
+		//?}
+		Operation<Void> original
+	) {
+		var previousTab = this.currentTab;
+
+		//? if >= 26.2 {
+		original.call(tab, playClickSound, flag);
+		//?} else {
+		/*original.call(tab, playClickSound);
+		*///?}
+
+		if (this.currentTab == previousTab) {
+			return;
+		}
+
+		if (previousTab instanceof StructurifyCategoryTab previousCategoryTab && previousCategoryTab.structurify$getToggleGroupsButton() != null) {
+			this.removeWidget.accept(previousCategoryTab.structurify$getToggleGroupsButton());
+		}
+
+		if (this.currentTab instanceof StructurifyCategoryTab currentCategoryTab && currentCategoryTab.structurify$getToggleGroupsButton() != null) {
+			this.addWidget.accept(currentCategoryTab.structurify$getToggleGroupsButton());
 		}
 	}
 }
